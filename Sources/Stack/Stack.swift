6a8be0c0f04dc972fb8bdb75d82f8865bf523991
jiggybear <cagri.sahan@gmail.com>
2017-11-10 22:55:48 -0600

    Copied from p2

---
+++ Sources/Stack/Stack.swift
@@ -0,0 +1,44 @@
+import LinkedList
+
+public class Stack<T: Equatable> {
+  let list: LinkedList<T>
+  var stackSize: Int = 0
+
+  public init() {
+    self.list = LinkedList<T> ()
+  }
+
+  public func push(item: T) throws {
+    do {
+      try self.list.insert(item: item, atIndex: 0)
+      self.stackSize += 1
+    }
+    catch {
+      throw error
+    }
+  }
+
+  public func pop() throws -> T {
+    let value = self.list[0]
+    do {
+      try self.list.remove(atIndex: 0)
+      self.stackSize -= 1
+    }
+    catch {
+      throw error
+    }
+    return value
+  }
+
+  public func peek() -> T {
+    return self.list[0]
+  }
+
+  public func isEmpty() -> Bool {
+    return self.stackSize == 0
+  }
+
+  public func size() -> Int {
+    return self.stackSize
+  }
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import StackTests
+
+XCTMain([
+    testCase(StackTests.allTests),
+])
